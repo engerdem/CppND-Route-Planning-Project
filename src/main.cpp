3ec1343b520880dbcfc2d5fdf125b191b1c38e5f
Mehmet E. Erdem <43347187+engerdem@users.noreply.github.com>
2021-03-25 22:43:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,20 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+
+    
+    std::cout << "Please type start x: " << "\n";
+    std::cin >> start_x;
+
+    std::cout << "Please type start y: " << "\n";
+    std::cin >> start_y;
+
+    std::cout << "Please type end x: " << "\n";
+    std::cin >> end_x;
+
+    std::cout << "Please type end y: " << "\n";
+    std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
